0f964bd5a82a52e5b754f77b0716fcfe8008d063
abderrazzak <92592274+obi1254@users.noreply.github.com>
2022-04-10 20:44:58 +0000

    hello b

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const cors = require('cors');
+const cheerio = require('cheerio');
+const axios = require('axios');
 const express = require('express');
 const getAllManga = require('./controllers/getAllMangas.js');
 const getMangaDetails = require('./controllers/getMangaDetails');
@@ -12,8 +14,32 @@ app.listen(port, () => {
   console.log(`running on port ${port} `);
 });
 
-app.get('/', (req, res) => {
-  res.send('Hello World');
+app.get('/', async (req, res) => {
+  const mangas = [];
+  try {
+    const { data } = await axios.get('https://teamx.fun/manga/');
+    const $ = cheerio.load(data);
+    //grabing the data
+    $('.thumb img').each((i, ele) => {
+      const img = $(ele).attr('src');
+      mangas.push({ img: img });
+    });
+    $('.thumb h3 a').each((i, ele) => {
+      const link = $(ele).attr('href');
+      const name = $(ele).text();
+      mangas[i].link = link;
+      mangas[i].name = name;
+    });
+
+    res.json(mangas);
+  } catch (error) {
+    console.log('error : ', error.message);
+  }
 });
-app.get('/details/:mangaName', getMangaDetails);
-app.get('/details/:mangaName/:chapter', getPics);
+
+app.get('/details/:mangaName', () => {
+  getMangaDetails();
+});
+app.get('/details/:mangaName/:chapter', () => {
+  getPics();
+});
