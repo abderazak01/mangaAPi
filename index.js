78c4f8a0103bf80a37a6e0df137ac5756f206956
abderrazzak <92592274+obi1254@users.noreply.github.com>
2022-04-11 23:38:54 +0000

    pushing throught github

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 const cors = require('cors');
-const cheerio = require('cheerio');
-const axios = require('axios');
 const express = require('express');
 const getAllManga = require('./controllers/getAllMangas.js');
 const getMangaDetails = require('./controllers/getMangaDetails');
@@ -14,32 +12,6 @@ app.listen(port, () => {
   console.log(`running on port ${port} `);
 });
 
-app.get('/', async (req, res) => {
-  const mangas = [];
-  try {
-    const { data } = await axios.get('https://teamx.fun/manga/');
-    const $ = cheerio.load(data);
-    //grabing the data
-    $('.thumb img').each((i, ele) => {
-      const img = $(ele).attr('src');
-      mangas.push({ img: img });
-    });
-    $('.thumb h3 a').each((i, ele) => {
-      const link = $(ele).attr('href');
-      const name = $(ele).text();
-      mangas[i].link = link;
-      mangas[i].name = name;
-    });
-
-    res.json(mangas);
-  } catch (error) {
-    console.log('error : ', error.message);
-  }
-});
-
-app.get('/details/:mangaName', () => {
-  getMangaDetails();
-});
-app.get('/details/:mangaName/:chapter', () => {
-  getPics();
-});
+app.get('/', getAllManga);
+app.get('/details/:mangaName', getMangaDetails);
+app.get('/details/:mangaName/:chapter', getPics);
